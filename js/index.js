2e4d957ad600c00f74505e035109e864f8cfe670
sh9801 <sh9801@naver.com>
2024-01-16 14:20:10 +0900

    css수정

--- js/index.js
+++ js/index.js
@@ -46,17 +46,19 @@ window.onload = function(){
     let skillSection = document.querySelector(".skill")
 
     // Start the progress bars with initial values
-    let proPs = progressBar(".ps", 0.6, "#1080E8");
-    let proAi = progressBar(".ai", 0.9, "#FF9A00");
-    let proAe = progressBar(".ae", 0.5, "#4B4BE1");
-    let proPr = progressBar(".pr", 0.6, "#5151CF");
-    let proId = progressBar(".id", 0.3, "#F6537F");
-    let proHtml = progressBar(".html", 0.5, "#FF835C");
-    let proCss = progressBar(".css", 0.6, "#52B2FF");
-    let proJs = progressBar(".js", 0.2, "#FFBD3D");
-    let proFigma = progressBar(".figma", 0.7, "#747474");
-    let proBlender = progressBar(".blender", 0.5, "#FF7021");
+    let proPs = progressBar(".ps", 0, "#1080E8");
+    let proAi = progressBar(".ai", 0, "#FF9A00");
+    let proAe = progressBar(".ae", 0, "#4B4BE1");
+    let proPr = progressBar(".pr", 0, "#5151CF");
+    let proId = progressBar(".id", 0, "#F6537F");
+    let proHtml = progressBar(".html", 0, "#FF835C");
+    let proCss = progressBar(".css", 0, "#52B2FF");
+    let proJs = progressBar(".js", 0, "#FFBD3D");
+    let proFigma = progressBar(".figma", 0, "#747474");
+    let proBlender = progressBar(".blender", 0, "#FF7021");
     observe.observe(skillSection)
+
+    // 영상 포폴
 }
 
 
